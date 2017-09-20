5d5e0bfb2e379d142061899a80a5211963dbb1a8
Romeo Maryns <romeo.maryns@gmail.com>
2017-09-20 15:56:14 +0200

    no message

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -10,8 +10,10 @@ import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class Application {
 
     final static String queueName = "spring-boot";
